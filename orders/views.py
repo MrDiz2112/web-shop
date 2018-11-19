947ffd0f8e113f08976a1d103683745670c61691
mrdiz <diz2012x@gmail.com>
2018-11-19 16:54:26 +0300

    Initial commit

---
+++ orders/views.py
@@ -0,0 +1,31 @@
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render, get_object_or_404
+from .models import OrderItem, Order
+from .forms import OrderCreateForm
+from cart.cart import Cart
+
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+
+
+def OrderCreate(request):
+    cart = Cart(request)
+
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+
+        if form.is_valid():
+            order = form.save()
+
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+            cart.clear()
+            return render(request, 'orders/order/created.html', {'order': order})
+
+    form = OrderCreateForm()
+
+    return render(request, 'orders/order/create.html', {'cart': cart,
+                                                        'form': form})
---
+++ shop/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.MainPage, name='MainPage'),
+    url(r'^catalog$', views.ProductList, name='ProductList'),
+    url(r'^(?P<category_slug>[-\w]+)/$', views.ProductList, name='ProductListByCategory'),
+    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.ProductDetail, name='ProductDetail'),
+    url(r'^contacts$', views.Contacts, name='Contacts')
+]
